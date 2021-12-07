f92f011812313e2457536f40dcb5b68b994fc13b
Thomas He <thomas.he34@gmail.com>
2021-12-06 23:58:28 -0500

    day6 complete

--- day6/run.py
+++ day6/run.py
@@ -1,22 +1,23 @@
+import numpy as np 
+
 # with open("sample.txt","r") as f: 
 with open("input.txt","r") as f: 
     for line in f: 
         line = line.strip().split(",")
 raw = [int(i) for i in line]
+raw = np.array(raw, dtype=np.int32)
 print("Initial state: {}".format(raw))
 
-end_day = 80
+end_day = 256
 day = 0
+count = np.array([0] * 9, dtype=np.int64)
+for i in raw: 
+    count[i] += 1
+
 while day < end_day: 
     day += 1
-    new_fish = []
-    for idx, value in enumerate(raw): 
-        raw[idx] -= 1
-        if raw[idx] < 0: 
-            raw[idx] = 6
-            new_fish.append(8)
-    raw += new_fish 
-    # print("After{}day: {}".format(day, raw))
-print("after {} days, there are a total of {} fish".format(day, len(raw)))
-
+    count = np.roll(count, -1)
+    count[6] += count[-1]
+    # print("after {} days, there are a total of {} fish".format(day, count))
+print("after {} days, there are a total of {} fish".format(day, np.sum(count)))
 print("end")
